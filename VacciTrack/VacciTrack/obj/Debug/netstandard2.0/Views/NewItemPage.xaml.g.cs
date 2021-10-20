292c0d8ee48f9c8fc46e5ddc8f51f85deb4d5898
Jinson Plakkal Joseph <jinsp@microsoft.com>
2021-10-20 19:38:45 +0530

    introduced datepicker

--- VacciTrack/VacciTrack/obj/Debug/netstandard2.0/Views/NewItemPage.xaml.g.cs
+++ VacciTrack/VacciTrack/obj/Debug/netstandard2.0/Views/NewItemPage.xaml.g.cs
@@ -17,12 +17,12 @@ namespace VacciTrack.Views {
     public partial class NewItemPage : global::Xamarin.Forms.ContentPage {
         
         [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
-        private global::Xamarin.Forms.DatePicker BirthDate;
+        private global::Xamarin.Forms.DatePicker DatePicker;
         
         [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
         private void InitializeComponent() {
             global::Xamarin.Forms.Xaml.Extensions.LoadFromXaml(this, typeof(NewItemPage));
-            BirthDate = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.DatePicker>(this, "BirthDate");
+            DatePicker = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.DatePicker>(this, "DatePicker");
         }
     }
 }
